6ce8e599cdda00a13f691ce1f94b0fdc5bd8c734
destinerikanb <destinerikanb@gmail.com>
2022-06-21 13:54:56 +0700

    [FEATURE] create body response for post method

--- server.js
+++ server.js
@@ -10,14 +10,25 @@ const requestListener = (request, response) => {
         response.end('<h1>Hello, this is GET method response!</h1>')
     }
     if(method === 'POST'){
-        response.end('<h1>Hi, this is POST method response!</h1>')
+        let body = [];
+
+        request.on('data', (chunk)=>{
+            body.push(chunk);
+        })
+
+        request.on('end', ()=>{
+            body = Buffer.concat(body).toString();
+            const {name} = JSON.parse(body);
+            response.end(`<h1>Hai, ${name}</h1>`)
+        })
+
     }
-    if(method === 'PUT'){
-        response.end('<h1>Bonjour, this is PUT method response!</h1>')
-    }
-    if(method === 'DELETE'){
-        response.end('<h1>Salam, this is DELETE method response!</h1>')
-    }
+    // if(method === 'PUT'){
+    //     response.end('<h1>Bonjour, this is PUT method response!</h1>')
+    // }
+    // if(method === 'DELETE'){
+    //     response.end('<h1>Salam, this is DELETE method response!</h1>')
+    // }
 }
 
 const server = http.createServer(requestListener);
