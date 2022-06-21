8505ca6911d6aaecb8021e7fcda1652bc2f3a643
destinerikanb <destinerikanb@gmail.com>
2022-06-21 09:05:19 +0700

    [FEATURE] create native http serrver with Node.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("Hello, we're going to learn how to create server!")
+const http = require('http');
+
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+    response.statusCode = 200;
+    response.end('<h1>Hello, This is HTTP Server!</h1>')
+}
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = 'localhost';
+
+server.listen(port, host, ()=>{
+    console.log(`Server is running on http://${host}:${port}`);
+})
