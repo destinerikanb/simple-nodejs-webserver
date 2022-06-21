a4f3449cb2761e0f68ddcc1473dd77192f4b197b
destinerikanb <destinerikanb@gmail.com>
2022-06-21 09:36:21 +0700

    [FEATURE] handling different request method with different response using request.method property

--- server.js
+++ server.js
@@ -3,13 +3,28 @@ const http = require('http');
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
     response.statusCode = 200;
-    response.end('<h1>Hello, This is HTTP Server!</h1>')
+
+    const {method} = request;
+
+    if(method === 'GET'){
+        response.end('<h1>Hello, this is GET method response!</h1>')
+    }
+    if(method === 'POST'){
+        response.end('<h1>Hi, this is POST method response!</h1>')
+    }
+    if(method === 'PUT'){
+        response.end('<h1>Bonjour, this is PUT method response!</h1>')
+    }
+    if(method === 'DELETE'){
+        response.end('<h1>Salam, this is DELETE method response!</h1>')
+    }
 }
 
 const server = http.createServer(requestListener);
 
 const port = 5000;
 const host = 'localhost';
+
 
 server.listen(port, host, ()=>{
     console.log(`Server is running on http://${host}:${port}`);
