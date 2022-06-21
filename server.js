ec3b400fd60a7c1fc6bbf656abe119a8a9477cd3
destinerikanb <destinerikanb@gmail.com>
2022-06-21 16:37:03 +0700

    create route to handling different path request

--- server.js
+++ server.js
@@ -4,31 +4,35 @@ const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
     response.statusCode = 200;
 
-    const {method} = request;
+    const {method, url} = request;
 
-    if(method === 'GET'){
-        response.end('<h1>Hello, this is GET method response!</h1>')
+    if(url === '/'){
+        if(method === 'GET'){
+            response.end('<h1>This is homepage</h1>')
+        } else {
+            response.end(`<h1>This page can't be accessed using ${method} request</h1>`)
+        }
     }
-    if(method === 'POST'){
-        let body = [];
 
-        request.on('data', (chunk)=>{
-            body.push(chunk);
-        })
-
-        request.on('end', ()=>{
-            body = Buffer.concat(body).toString();
-            const {name} = JSON.parse(body);
-            response.end(`<h1>Hai, ${name}</h1>`)
-        })
-
-    }
-    // if(method === 'PUT'){
-    //     response.end('<h1>Bonjour, this is PUT method response!</h1>')
-    // }
-    // if(method === 'DELETE'){
-    //     response.end('<h1>Salam, this is DELETE method response!</h1>')
-    // }
+    else if(url === '/about') {
+        if(method === 'GET') {
+            response.end('<h1>Hello! This is page about</h1>')
+        } else if(method === 'POST') {
+            let body = [];
+            request.on('data', (chunk)=>{
+                body.push(chunk)
+            })
+            request.on('end', ()=>{
+                body = Buffer.concat(body).toString();
+                const {name} = JSON.parse(body)
+                response.end(`<h1>Hello, ${name}! This is page about</h1>`)
+            })
+        } else {
+                response.end(`<h1>This page can't be accessed using ${method} request</h1>`)
+        }
+    } else {
+        response.end('<h1>Page not found</h1>')
+    } 
 }
 
 const server = http.createServer(requestListener);
