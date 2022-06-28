942c115aa072b07830004cf7d2a1beeb08da2b15
destinerikanb <destinerikanb@gmail.com>
2022-06-28 11:39:25 +0700

    [FEATURE] able to write data to response body

--- server.js
+++ server.js
@@ -9,17 +9,26 @@ const requestListener = (request, response) => {
     if(url === '/'){
         if(method === 'GET'){
             response.statusCode = 200;
-            response.end('<h1>This is homepage</h1>')
+            // response.end('<h1>This is homepage</h1>')
+            response.end(JSON.stringify({
+                message : 'This is homepage',
+            }))
         } else {
             response.statusCode = 400;
-            response.end(`<h1>This page can't be accessed using ${method} request</h1>`)
+            // response.end(`<h1>This page can't be accessed using ${method} request</h1>`)
+            response.end(JSON.stringify({
+                message : `This page can't be accessed using ${method} request`,
+            }))
         }
     }
 
     else if(url === '/about') {
         if(method === 'GET') {
             response.statusCode = 200;
-            response.end('<h1>Hello! This is page about</h1>')
+            // response.end('<h1>Hello! This is page about</h1>')
+            response.end(JSON.stringify({
+                message : 'Hello! This is page about',
+            }))
         } else if(method === 'POST') {
             let body = [];
             request.on('data', (chunk)=>{
@@ -29,15 +38,27 @@ const requestListener = (request, response) => {
                 body = Buffer.concat(body).toString();
                 const {name} = JSON.parse(body)
                 response.statusCode = 200;
-                response.end(`<h1>Hello, ${name}! This is page about</h1>`)
+                // response.end(`<h1>Hello, ${name}! This is page about</h1>`)
+                response.end(JSON.stringify({
+                    message : `Hello, ${name}! This is page about`,
+                }))
             })
         } else {
                 response.statusCode = 400;
-                response.end(`<h1>This page can't be accessed using ${method} request</h1>`)
+                // response.end(`<h1>This page can't be accessed using ${method} request</h1>`)
+                response.end(JSON.stringify({
+                    message : `This page can't be accessed using ${method} request`,
+                }))
         }
     } else {
         response.statusCode = 404;
-        response.end('<h1>Page not found</h1>')
+        // response.end('<h1>Page not found</h1>')
+        //Menuliskan data JSON pada response body
+        //Karena response.end() menerima string(buffer) maka kita perlu mengubah objek JavaScript menjadi JSON string
+        //menggunakan JSON.stringify
+        response.end(JSON.stringify({
+            message : 'Page not found',
+        }))
     } 
 }
 
